dec7f69917a50917d275a352ccfae40f0db2c293
saifxthetic <aliamirsaifs@gmail.com>
2024-12-27 17:31:23 +0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    int id;
+    String name;
+    double amount;
+
+    Customer(int id, String name, double amount) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+    }
+}
